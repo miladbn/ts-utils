718eece2002338513c9b16931edff4cb176564ae
milad banajami <40021606+miladbn@users.noreply.github.com>
2024-04-24 13:24:01 +0330

    adding gregorianToJalali

--- date-utlis.ts
+++ date-utlis.ts
@@ -13,3 +13,17 @@ export const jalaliToGregorian = (
     }
   }
   
+
+export const gregorianToJalali = (
+    gregorian: Date | string,
+    hasTime: boolean = false
+  ): string => {
+    gregorian = new Date(gregorian)
+    gregorian = `${gregorian.getFullYear()}-${
+      gregorian.getMonth() + 1
+    }-${gregorian.getDate()} ${gregorian.getHours()}:${gregorian.getMinutes()}:${gregorian.getSeconds()}`
+    return moment(`${gregorian}`, 'YYYY-MM-DD HH:mm:ss').format(
+      hasTime ? 'jYYYY/jMM/jDD HH:mm:ss' : 'jYYYY/jMM/jDD'
+    )
+  }
+  
